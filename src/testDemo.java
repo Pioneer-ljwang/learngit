99db077c45f84432c4f32fcfba26402f8c454c5c
wlj1582437140 <1582437140@qq.com>
2016-09-21 16:15:06 +0800

    更改一下

--- src/testDemo.java
+++ src/testDemo.java
@@ -1,5 +1,8 @@
 /**
  * Created by ${ljwang} on 2016/9/21.
  */
-public class testDemo {
+public class testDemo{
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
 }
